1e51d541331de550a954038ead2c9e51ceda3318
Robaire Galliath <galliath.robaire@gmail.com>
2024-10-27 11:47:28 -0400

    Use mvu.py

--- OS4_Q1.py
+++ OS4_Q1.py
@@ -1,46 +1,7 @@
 # Robaire Galliath
 # EM 411, Fall 2024
 
-import numpy as np
-import math
-from dataclasses import dataclass
-
-
-@dataclass
-class Utility:
-    attribute: list[float]
-    utility: list[float]
-
-    def util(self, x):
-        return np.interp(x, self.attribute, self.utility)
-
-
-class MVU:
-    utilities: list[Utility]
-    weights: list[float]
-
-    def __init__(self, utilities, weights):
-
-        if not math.isclose(np.sum(weights), 1):
-            raise ValueError("Weights must sum to 1")
-
-        if len(utilities) != len(weights):
-            raise ValueError(
-                "Utilities and Weights must have the same number of elements"
-            )
-
-        self.utilities = utilities
-        self.weights = weights
-
-    def evaluate(self, attributes):
-
-        if len(attributes) != len(self.utilities):
-            raise ValueError(f"Attributes must be length {len(self.utilities)}")
-
-        return np.dot(
-            [u.util(a) for a, u in zip(attributes, self.utilities)], self.weights
-        )
-
+from mvu import Utility, MVU
 
 # Single Variate Utility Functions
 volume = Utility([0, 500, 1000, 1500, 2000], [0, 0.2, 0.4, 0.8, 1.0])
