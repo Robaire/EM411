bc57bd40a522fc9d17e853ec710be17cec774f02
Robaire Galliath <galliath.robaire@gmail.com>
2024-10-29 18:48:04 -0400

    Update

--- transport.py
+++ transport.py
@@ -1,4 +1,5 @@
 # Robaire Galliath
+# EM 411, Fall 2024
 
 
 from dataclasses import dataclass
@@ -22,12 +23,12 @@ mvu = MVU(utilities, weights)
 # Class for tracking vehicle state
 class RealVehicle:
     vehicle: _Vehicle
-    next_available: float  # The time this vehicle becomes available [hr]
     battery_capacity: float  # Current battery charge [kWh]
+    next_available: float = 0  # The time this vehicle becomes available [hr]
+    ride_count: int = 0
 
     def __init__(self, vehicle):
         self.vehicle = vehicle
-        self.next_available = 0
         self.battery_capacity = self.vehicle.battery.capacity
 
     def range(self):
@@ -36,7 +37,7 @@ class RealVehicle:
 
     def move(self, distance):
         """Decrease the battery capacity for a distance traveled in km."""
-        self.battery_capacity -= (v.vehicle.power_consumption() * distance) / 1000
+        self.battery_capacity -= (self.vehicle.power_consumption() * distance) / 1000
         return self.battery_capacity
 
     def charge_time(self):
@@ -51,9 +52,67 @@ class RealVehicle:
 class Ride:
     distance: float  # One way ride distance
     passengers: int  # Passenger count
-    start_time: float
-    filled_time: float = None
-    complete_time: float = None
+    start_time: float  # When the request begins [hr]
+    filled_time: float = None  # When the request is filled [hr]
+    complete_time: float = None  # When the ride is completed [hr]
+
+    def wait_time(self):
+        """Wait time [hr]."""
+        return self.filled_time - self.start_time
+
+    def travel_time(self):
+        """Travel time [hr]."""
+        return self.complete_time - self.filled_time
+
+
+class Result:
+    vehicles: list[_Vehicle]
+    fleet: Fleet
+    total_requests: int
+    completed: int
+    dropped: int
+    impossible: int
+    pax_volume: int
+    average_wait: float
+    max_wait: float
+    average_duration: float
+    average_distance: float
+    availability: float
+
+    utility: float
+
+    def __init__(self, rides, vehicles, fleet):
+        self.vehicles = vehicles
+        self.fleet = fleet
+
+        self.total_requests = len(rides)
+
+        completed = [r for r in rides if r.filled_time]
+        self.completed = len(completed)
+
+        dropped = [r for r in rides if r.complete_time == -1]
+        self.dropped = len(dropped)
+
+        impossible = [r for r in rides if not r.complete_time]
+        self.impossible = len(impossible)
+
+        self.pax_volume = sum([r.passengers for r in completed])
+
+        wait_times = [r.wait_time() * 60 for r in completed]
+        self.average_wait = sum(wait_times) / len(wait_times)
+        self.max_wait = max(wait_times)
+
+        trip_times = [r.travel_time() * 60 for r in completed]
+        self.average_duration = sum(trip_times) / len(trip_times)
+
+        trip_dist = [r.distance for r in completed]
+        self.average_distance = sum(trip_dist) / len(trip_dist)
+
+        self.availability = len([w for w in wait_times if w < 1]) / len(rides)
+
+        self.utility = mvu.evaluate(
+            [self.pax_volume, 0, self.average_wait, self.availability]
+        )
 
 
 ####################################################
@@ -66,7 +125,7 @@ class Ride:
 #   - All trips originate at Kendall/MIT
 #   - Trips have an average distance of 1.5 km, std-dev 0.4km
 #   - Vehicles return to Kendall/MIT after dropping off passengers
-#   - Trips have 1-4 passengers
+#   - Trips have 1-6 passengers
 #   - Speed limit on surface streets is 40 kph (~25 mph)
 #   - Speed limit for bikes is 15 kph (~10 mph)
 #
@@ -81,17 +140,101 @@ class Ride:
 random.seed("EM411")  # Seed the RNG
 
 # Distance Model
-distance = lambda: random.gauss(1.5, 0.4)  # 1.5 km average, 0.4 km std-dev
+DISTANCE = lambda: random.gauss(1.5, 0.4)  # 1.5 km average, 0.4 km std-dev
 # distance = lambda: random.gauss(5, 2)  # 5 km average, 2 km std-dev
 # Passenger Model
-passengers = lambda: max(round(random.lognormvariate(0, 0.5)), 1)
+PASSENGERS = lambda: max(round(random.lognormvariate(0, 0.5)), 1)
+
+# Demand Model
+# DEMAND_ADJUST = lambda x: random.gauss(x, x * 0.05)  # std-dev 5% of mean
+DEMAND_ADJUST = lambda x: x
 
 # Demand over a 24 hour period, evenly spaced
 DEMAND = [15, 5, 15, 50, 150, 150, 150, 100, 75, 100, 50, 35]
+# DEMAND = [5000]  # Saturating demand
 
+MAX_WAIT = 10 / 60  # [hr] maximum wait allowed for a ride request or its dropped
 DWELL_TIME = 1 / 60  # [hr] (one minute)
 CHARGE_DISTANCE = 5  # [km] start charging when range drops below this value
 CHARGE_TIME_PENALTY = 0.25  # [hr] fixed time penalty for charging
+
+
+def run_sim(fleet):
+    """Return a Result"""
+
+    ####################
+    # Simulation Setup #
+    ####################
+
+    # Randomly generate a list of ride requests over a 24 hour period
+    rides: list[Ride] = []
+    for i, demand in enumerate(DEMAND):
+        interval = 24 / len(DEMAND)
+
+        for _ in range(math.ceil(DEMAND_ADJUST(demand))):
+            time = random.uniform(i * interval, (i + 1) * interval)
+            rides.append(Ride(DISTANCE(), PASSENGERS(), time))
+
+    # Sort rides by start time
+    rides = sorted(rides, key=lambda x: x.start_time)
+
+    # Build a list of real vehicles for the simulation
+    vehicles: list[RealVehicle] = []
+    for v, q in zip(fleet.vehicles, fleet.quantities):
+        for _ in range(q):
+            vehicles.append(RealVehicle(v))
+
+    ###################
+    # Simulation Loop #
+    ###################
+    for ride in rides:
+
+        # Find the next available vehicle that meets the ride criteria
+        vehicles = sorted(
+            vehicles,
+            key=lambda x: (
+                x.next_available,
+                x.battery_capacity,
+                x.vehicle.chassis.pax,
+            ),
+        )
+        for v in vehicles:
+
+            # Check the vehicle has enough room and distance to complete the ride
+            if (
+                ride.passengers <= v.vehicle.chassis.pax
+                and v.range() >= ride.distance * 2
+            ):
+
+                # Check if this ride will be filled within the max wait time
+                if max(ride.start_time, v.next_available) - ride.start_time > MAX_WAIT:
+                    # Drop the ride
+                    ride.complete_time = -1
+                    break
+
+                # One way travel time
+                travel_time = (ride.distance / v.vehicle.speed()) + DWELL_TIME
+
+                # Update ride parameters
+                ride.filled_time = max(ride.start_time, v.next_available)
+                ride.complete_time = ride.filled_time + travel_time
+
+                # Update vehicle parameters
+                v.next_available = ride.filled_time + travel_time * 2  # Availability
+                v.ride_count += 1  # Increase the vehicle's ride count
+                v.move(ride.distance * 2)  # Update the battery charge
+                if v.range() <= CHARGE_DISTANCE:
+                    v.next_available += v.charge_time() + CHARGE_TIME_PENALTY
+                    v.battery_capacity = v.vehicle.battery.capacity  # Reset the battery
+                # Could make a decision to charge based on the availability of all other vehicles
+
+                break
+
+    ###################
+    # Analyze Results #
+    ###################
+    return Result(rides, vehicles, fleet)
+
 
 #################
 # Design Vector #
@@ -105,86 +248,40 @@ car1 = RoadVehicle(
     car_autonomy["A3"],
 )
 
-fleet = Fleet([car1], [15])
+car2 = RoadVehicle(
+    car_chassis["C2"],
+    car_batteries["P1"],
+    car_chargers["G1"],
+    car_motors["M2"],
+    car_autonomy["A3"],
+)
 
-####################
-# Simulation Setup #
-####################
 
-# Randomly generate a list of ride requests over a 24 hour period
-rides: list[Ride] = []
-for i, demand in enumerate(DEMAND):
-    interval = 24 / len(DEMAND)
+fleets: list[Fleet] = []
+fleets.append(Fleet([car1], [15]))
+fleets.append(Fleet([car1, car2], [10, 5]))
 
-    # TODO: Randomly adjust the demand vector
-    for _ in range(math.ceil(demand)):
-        time = random.uniform(i * interval, (i + 1) * interval)
-        rides.append(Ride(distance(), passengers(), time))
+# TODO: Programmatically build fleets
 
-# Sort rides by start time
-rides = sorted(rides, key=lambda x: x.start_time)
+results: list[Result] = []
+for fleet in fleets:
+    results.append(run_sim(fleet))
 
-# Build a list of real vehicles for the simulation
-vehicles: list[RealVehicle] = []
-for v, q in zip(fleet.vehicles, fleet.quantities):
-    for _ in range(q):
-        vehicles.append(RealVehicle(v))
+r = run_sim(fleet)
 
-###################
-# Simulation Loop #
-###################
-for ride in rides:
-
-    # Find the next available vehicle that meets the ride criteria
-    vehicles = sorted(vehicles, key=lambda x: (x.next_available, x.battery_capacity))
-    for v in vehicles:
-
-        # Check the vehicle has enough room and distance to complete the ride
-        if ride.passengers <= v.vehicle.chassis.pax and v.range() >= ride.distance * 2:
-
-            # Set the fill time for the ride
-            ride.filled_time = max(ride.start_time, v.next_available)
-
-            # One way travel time
-            travel_time = (ride.distance / v.vehicle.speed()) + DWELL_TIME
-
-            # Set the completed time for the ride
-            ride.complete_time = ride.filled_time + travel_time
-
-            # Set the next availability for the vehicle
-            v.next_available = ride.filled_time + travel_time * 2
-
-            # Adjust the battery capacity
-            v.move(ride.distance * 2)
-
-            # If the vehicle needs to charge afterwards, set the availability
-            if v.range() <= CHARGE_DISTANCE:
-                v.next_available += v.charge_time() + CHARGE_TIME_PENALTY
-                v.battery_capacity = v.vehicle.battery.capacity  # Reset the battery
-
-            # Could make a decision to charge based on the availability of all other vehicles
-
-            break
-
-##################
-# Print Analysis #
-##################
-
-print(f"{len(rides)} ride requests")
-print(f"{len(vehicles)} fleet vehicles")
-
-completed_rides = [r for r in rides if r.complete_time]
-print(f"{len(completed_rides)} rides completed")
-
-pax_volume = sum([r.passengers for r in rides if r.complete_time])
-print(f"Passenger Volume: {pax_volume}")
-
-wait_times = [
-    (r.filled_time - r.start_time) * 60 for r in rides if r.filled_time
-]  # [min]
-print(f"Average Wait: {sum(wait_times) / len(wait_times)} minutes")
-print(f"Max Wait: {max(wait_times)} minutes")
-
-# The fraction of rides completed with less than 1 min wait time
-availability = len([w for w in wait_times if w < 1]) / len(rides)
-print(f"Availability: {availability}")
+print(f"{len(r.vehicles)} fleet vehicles")
+print()
+print(f"Total Requests: {r.total_requests}")
+print(f"Completed Rides: {r.completed}")
+print(f"Dropped Rides: {r.dropped}")
+print(f"Impossible Rides: {r.impossible}")
+print()
+print(f"Passenger Volume: {r.pax_volume}")
+print(f"Average Wait: {r.average_wait} min")
+print(f"Max Wait: {r.max_wait} min")
+print()
+print(f"Average Trip Distance: {r.average_distance} km")
+print(f"Average Trip Duration: {r.average_duration} min")
+print()
+print(f"Availability: {r.availability}")
+print(f"Utility: {r.utility}")
